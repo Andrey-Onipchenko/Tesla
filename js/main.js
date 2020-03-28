f23546f462cda91feb5a804840e167c9d5021ac3
Andrey <Andreysk8er@ukr.net>
2020-03-28 17:10:10 +0200

    modal

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
+const button = document.querySelector(".button");
+
+button.addEventListener("click", function() {
+  swal({
+    text:
+      "Оставьте, пожалуйста, ваш номер телефона, наш менеджер обязательно с вами свяжется.",
+    content: "input",
+    buttons: ["Отмена", "Хочу тест драйв"]
+  });
+});
+
+const footerBtn = document.querySelector(".footer-btn");
+footerBtn.addEventListener("click", function() {
+  swal({
+    text:
+      "Оставьте, пожалуйста, ваш номер телефона, наш менеджер обязательно с вами свяжется.",
+    content: "input",
+    buttons: ["Передумал", "Хочу тест драйв"]
+  });
+});
 $(function() {
   $(".slider").slick({
     arrows: false,
